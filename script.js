394e8945e3fab14485bf2d06dc3588604fc04eb7
Isbat Ullah <ullah.isbat@gmail.com>
2023-07-20 14:21:03 -0400

    front end code

---
+++ script.js
@@ -0,0 +1,27 @@
+// script.js
+document.getElementById('emailForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+  
+    const email = event.target.elements.email.value;
+    const data = { email };
+  
+    try {
+      const response = await fetch('/api/storeEmail', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+  
+      if (response.ok) {
+        // Redirect to another link after successful submission
+        window.location.href = 'https://aianswer.us'; // Replace with your desired link
+      } else {
+        alert('Failed to save email address. Please try again later.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Failed to save email address. Please try again later.');
+    }
+  });
