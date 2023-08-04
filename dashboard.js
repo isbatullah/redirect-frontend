30d4ddff66bd7565b1f7e78e3b2532783ed988d6
Isbat Ullah <ullah.isbat@gmail.com>
2023-08-04 16:41:23 -0400

    restructuring

--- dashboard.js
+++ dashboard.js
@@ -10,3 +10,31 @@ document.getElementById('urlForm').addEventListener('submit', function(event) {
     // Show a confirmation message (optional)
     alert('URL saved successfully!');
   });
+
+
+  document.getElementById('linkRedirectorForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+  
+    const subdirectory = event.target.elements.subdirectory.value;
+    const redirectLink = event.target.elements.redirectLink.value;
+  
+    try {
+      const response = await fetch('/api/createRedirector', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ subdirectory, redirectLink }),
+      });
+  
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log(responseData); // Confirmation message or additional data from the server
+      } else {
+        console.error('Failed to create redirector.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Failed to create redirector. Please try again later.');
+    }
+  });
--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-// script.js
-document.getElementById('emailForm').addEventListener('submit', async function(event) {
-    event.preventDefault();
+// // script.js
+// document.getElementById('emailForm').addEventListener('submit', async function(event) {
+//     event.preventDefault();
   
-    const email = event.target.elements.email.value;
-    const data = { email };
-    console.log(email)
-    console.log(data)
+//     const email = event.target.elements.email.value;
+//     const data = { email };
+//     console.log(email)
+//     console.log(data)
   
-    try {
-      const response = await fetch('https://redirect-backend.onrender.com/api/storeEmail', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
+//     try {
+//       const response = await fetch('https://redirect-backend.onrender.com/api/storeEmail', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(data),
+//       });
   
-      if (response.ok) {
-        // Read the redirect URL from local storage
-        const redirectURL = localStorage.getItem('redirectURL');
-        if (redirectURL) {
-          window.location.href = redirectURL;
-        } else {
-          // If no redirect URL is set, use a default URL
-          window.location.href = 'https://aianswer.us'; // Replace with your desired link
-        }
-      } else {
-        alert('Failed to save email address. Please try again later.');
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      alert('Failed to save email address. Please try again later.');
-    }
-  });
+//       if (response.ok) {
+//         // Read the redirect URL from local storage
+//         const redirectURL = localStorage.getItem('redirectURL');
+//         if (redirectURL) {
+//           window.location.href = redirectURL;
+//         } else {
+//           // If no redirect URL is set, use a default URL
+//           window.location.href = 'https://aianswer.us'; // Replace with your desired link
+//         }
+//       } else {
+//         alert('Failed to save email address. Please try again later.');
+//       }
+//     } catch (error) {
+//       console.error('Error:', error);
+//       alert('Failed to save email address. Please try again later.');
+//     }
+//   });
