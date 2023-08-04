5990d0ff60d289bef94b8793897b4688a2f87442
Isbat Ullah <ullah.isbat@gmail.com>
2023-08-04 16:48:51 -0400

    bug fix

--- dashboard.js
+++ dashboard.js
@@ -1,25 +1,11 @@
-// settings.js
-document.getElementById('urlForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    const newURL = event.target.elements.newURL.value;
-  
-    // Save the new URL in local storage for later use
-    localStorage.setItem('redirectURL', newURL);
-  
-    // Show a confirmation message (optional)
-    alert('URL saved successfully!');
-  });
-
-
-  document.getElementById('linkRedirectorForm').addEventListener('submit', async function(event) {
+document.getElementById('linkRedirectorForm').addEventListener('submit', async function(event) {
     event.preventDefault();
   
     const subdirectory = event.target.elements.subdirectory.value;
     const redirectLink = event.target.elements.redirectLink.value;
   
     try {
-      const response = await fetch('/api/createRedirector', {
+      const response = await fetch('https://redirect-backend.onrender.com/api/createRedirector', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
